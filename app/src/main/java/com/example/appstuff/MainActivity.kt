1168b594b4b1c34ff68caddc3cebdd7506fa5a85
tehepix2 <daniel12landa@gmail.com>
2023-10-26 15:22:12 -0400

    cool stuff

--- app/src/main/java/com/example/appstuff/MainActivity.kt
+++ app/src/main/java/com/example/appstuff/MainActivity.kt
@@ -3,14 +3,21 @@ package com.example.appstuff
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.appstuff.ui.theme.AppStuffTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,24 +27,44 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Greeting("Android")
+                    Encouragement("egg")
+
                 }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
+ fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(color = Color.Transparent) {
+        Text(
             text = "Hello $name!",
-            modifier = modifier
-    )
+            modifier = modifier.padding(horizontal = 40.dp, vertical = 200.dp).offset(x = 0.dp, y = 50.dp).background(color = Color.Red)
+        )
+    }
 }
+@Composable
+fun Encouragement(name: String, modifier: Modifier = Modifier) {
 
+    Surface(color = Color.Cyan){
+        Text(
+            text = "You can do this, $name!",
+            modifier = modifier
+                .padding(horizontal = 20.dp, vertical = 20.dp)
+
+        )
+    }
+
+}
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    AppStuffTheme {
-        Greeting("Android")
-    }
+
+        AppStuffTheme {
+            Greeting("user")
+            Encouragement("egg")
+        }
+    
 }
